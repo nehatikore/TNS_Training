87997935c71c2ef6b2760968586c2bbf1f0b5ae2
nehatikore <nehatikore0309@gmail.com>
2025-07-05 11:24:28 +0530

    Add files via upload

---
+++ EvenOddDemo.java
@@ -0,0 +1,11 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int number = 10;  // You can change this value to test with other numbers
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an Even number.");
+        } else {
+            System.out.println(number + " is an Odd number.");
+        }
+    }
+}
