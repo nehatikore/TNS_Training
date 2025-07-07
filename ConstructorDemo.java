8d3efc5ac43be8651e0202e8d65776b52900eb90
nehatikore <nehatikore0309@gmail.com>
2025-07-07 10:50:54 +0530

    Add files via upload

---
+++ ConstructorDemo.java
@@ -0,0 +1,41 @@
+public class Customer{
+   
+           //Default Constructor
+ 
+            public Customer(){
+       
+                      System.out.println("Default Constructor.......");
+  
+      
+    }
+   
+ //parameterized Constructor
+ 
+          public Customer(int id,String name,int age){
+    
+                            System.out.println("Parameterized Constructor called.....");
+                                  System.out.println(id+" "+name+" "+age);
+
+    }
+   
+   public static void main(String[] args)
+  
+   {
+       
+              Customer c1=new Customer();
+     
+              Customer c2=new Customer(101,"neha",22);
+ 
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
