edace88e851d5da8ffc63a30ed6cf1c59166de04
nehatikore <nehatikore0309@gmail.com>
2025-07-14 18:05:15 +0530

    Add files via upload

---
+++ FinalVariable.java
@@ -0,0 +1,7 @@
+public class FinalExample {
+    public static void main(String[] args) {
+        final int number = 10; // final variable
+        //number=70;  cannot assign a value to final variable number
+        System.out.println("The number is: " + number);
+    }
+}
