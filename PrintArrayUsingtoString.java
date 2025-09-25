677db1ac0d6b3deb3aac52e106c915e0c5d10c98
nehatikore <nehatikore0309@gmail.com>
2025-09-25 17:36:03 +0530

    Add files via upload

---
+++ PrintArrayUsingtoString.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ ReverseNumber.java
@@ -0,0 +1,11 @@
+public class ReverseNumber {
+    public static void main(String[] args) {
+        int num = 1234, rev = 0;
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num = num / 10;
+        }
+        System.out.println("Reverse: " + rev);
+    }
+}
+// Logic: Extract digits and build the reverse.
